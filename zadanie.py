f4d9ff9ae9dd657d360f0b649969950eddac5897
CreativeFun69 <92749389+CreativeFun@users.noreply.github.com>
2022-07-14 14:45:29 +0200

    download works search problem works

--- zadanie.py
+++ zadanie.py
@@ -8,50 +8,54 @@ from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import os.path
 
 """ustawienie dodatkowych opcji na pominięcie komunikatu harmness file (z drugiej strony można by to dać w metodę download ale wtedy by należało robić nowego drivera
 a zależało w sumie mi na ciągłości okna bo ładniej wygląda)"""
 
 Options = webdriver.ChromeOptions()
-prefs = {'safebrowsing.enabled': 'false'}
+prefs = {
+        'safebrowsing.enabled': 'false',
+         }
 Options.add_experimental_option("prefs", prefs)
 
-#stałe z linkami do stron oraz czasami
+#stałe z linkami do stron itp
 selenium_main = "https://www.selenium.dev"
 selenium_downloads = "https://www.selenium.dev/downloads/"
 loading_time = 5
 truesecond = 1
 driver = webdriver.Chrome(options=Options)
 
+
 def constant_wait():
     constant_wait = 3
+    print("Waiting for site to fully load, time left:", end=" ")
     for i in range(constant_wait):
-        print("Waiting for site to fully load, time left {} (s)".format(constant_wait - i))
+        print("{} (s)...".format(constant_wait - i), end="")
         time.sleep(truesecond)
-    print("PASS")
+    print("\n")
     return time.sleep(constant_wait)
 
 def pageopen(url):
-    print("Trying to get {}".format(url))
+    print("Trying to load {}".format(url))
     try:
         driver.get(url)
         driver.maximize_window()
-        print("Waiting {} s to load the site".format(loading_time))
+        print("Waiting {} s to load the site: ".format(loading_time), end="")
         for i in range(loading_time):
-            print("Time left {}".format(loading_time - i))
+            print("{} (s)...".format(loading_time - i), end="")
             time.sleep(truesecond)
-        print("PASS")
+        print("\n *PASS*\n")
     except Exception as e:
         print("FAIL")
         print(e.message)
         driver.close()
 
 def searchphrase():
-    string = "XDDDDDD"
+    string = "dowolne hasło"
     try:
         searchbar = driver.find_element(By.ID, 'docsearch')
         searchbar.click()
-        constant_wait()
         try:
             text = driver.find_element(By.ID, 'docsearch-input')
             text.click()
@@ -73,22 +77,32 @@ def download(url):
         pageopen(url)
         gotit = driver.find_element(By.XPATH, '/html/body/div/main/div[2]/div[2]/div/div/p[1]/a')
         gotit.click()
-
         constant_wait()
-        #tu powinno być sprawdzenie czy ten plik się pobrał tak naprawdę jakiś wait czy coś w ten deseń
-
         print("Download file test case successfully completed")
+
     except Exception as e:
         print("FAIL")
         print(e.message)
         driver.close()
 
-print("Selenium site check things test start")
+print("**************************************")
+print("Selenium site check things test start \n")
 
-if(pageopen(selenium_main)):
-    print("opening selenium site test sucesfully completed")
+print("======================================")
+print("Test 1, opening selenium page")
+print("====================================== \n")
+pageopen(selenium_main) == True
+
+print("======================================")
+print("Test 2, searching for phrase")
+print("====================================== \n")
 searchphrase()
+
+print("======================================")
+print("Test 3, downloading file Selenium Server(Grid) v.4.3.0")
+print("====================================== \n")
 download(selenium_downloads)
 
-constant_wait()
+
+print("\n Closing browser")
 driver.close()
